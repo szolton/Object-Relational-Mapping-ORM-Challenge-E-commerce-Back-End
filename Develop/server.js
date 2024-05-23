76d3f76153942eb35544f65b1c934ec5d2be24c8
SLZolton <slzolton@SLZolton-27.local>
2024-05-23 10:39:28 -0400

    got the npm run seed command to work

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
