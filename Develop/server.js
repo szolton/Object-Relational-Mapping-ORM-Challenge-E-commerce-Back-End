228962884640a84c2561a3bc661d63ba3565cf93
SLZolton <slzolton@SLZolton-27.local>
2024-05-23 11:15:20 -0400

    got the npm start and npm run seed commands to work from the command line

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
-);
+const sequelize = new Sequelize('ecommerce', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1); // Exit the process with an error code
+  });
 
 // Sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
